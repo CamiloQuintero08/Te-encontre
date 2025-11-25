83b59f6fbcb5aae1893d8afcb798439933319fc8
Juan <jcrestrepo810@gmail.com>
2025-11-24 22:30:58 -0500

    se trabajo en el diseño de la app

---
+++ src/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "node_modules/flowbite/**/*.js"
+  ],
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
